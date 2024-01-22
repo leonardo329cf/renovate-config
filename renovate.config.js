dfea0912e7d0a4345a5f14b271e8e5a46c7e0219
Leonardo Ferreira <leonardo329cf@gmail.com>
2024-01-22 10:03:59 -0300

    Add configurations for angular patch, major 12 and 13

--- renovate.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  endpoint: 'https://api.bitbucket.org/',
-  token: '',
-  platform: 'bitbucket',
-  onboardingConfig: {
-    extends: ['config:recommended', 'github>leonardo329cf/renovate-config'],
-  },
-  repositories: ['leonardocardozoferreira/jhipsterapp'],
-};
